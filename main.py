2f6a8c3ca2ec6521bee55ef0dbb3ffc0bf48fbd4
Oye-sheezzi <testinguse3141@gmail.com>
2024-10-16 11:49:00 +0500

    add one new python code

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-print("First project for github")
-print("Second line")
+class database:
+    def __init__(self, name):
+        print("You have created an object")
+        print("Yiur name is: ", name)
+
+obj = database("sheezzi")
